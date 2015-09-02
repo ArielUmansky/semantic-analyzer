024afa504005f87520b11f32613f9554cf55966f
Ariel <umanskyariel@gmail.com>
2015-09-02 04:19:04 -0300

    Implements vector spaces calculations

--- app/models/KMeans/corpus.rb
+++ app/models/KMeans/corpus.rb
@@ -1,16 +1,15 @@
 class Corpus
 
-  def initialize(array_of_documents_as_strings)
-    validate_documents(array_of_documents_as_strings)
-    initialize_document_list(array_of_documents_as_strings)
+  MEANINGLESS_CHARS = ",."
+
+  def initialize(input_data)
+    validate_documents(input_data)
+    array_of_strings = input_data #Because the rest has a side effect on the string
+    initialize_document_list(array_of_strings)
   end
 
   def document_vector_list
     @document_vector_list
-  end
-
-  def add_to_document_vector_list(document_vector)
-    @document_vector_list.add(document_vector)
   end
 
   def count
@@ -23,34 +22,42 @@ class Corpus
 
   private
 
-    def initialize_document_list(array_of_documents_as_strings)
+    def initialize_document_list(array_of_strings)
+
+      @set_of_terms = initialize_set_of_terms(array_of_strings)
+
+      @documents = array_of_strings
+
       @document_vector_list = Array.new
-      array_of_documents_as_strings.each do |document|
+      array_of_strings.each do |document|
         document_vector = DocumentVector.new(document)
-        document_vector.vector_space = calculate_vector_space(document)
+        calculate_vector_space!(document_vector)
         @document_vector_list << document_vector
       end
     end
 
-    def validate_documents(array_of_documents_as_strings)
-      if array_of_documents_as_strings.nil? || !array_of_documents_as_strings.is_a?(Array)
+    def initialize_set_of_terms(array_of_strings)
+      Set.new(array_of_strings.map { |d| d.tr!((MEANINGLESS_CHARS), ""); d.split (' ')}.flatten)
+    end
+
+    def validate_documents(array_of_strings)
+      if array_of_strings.nil? || !array_of_strings.is_a?(Array)
         raise Analyzer::WRONG_INPUT_EXCEPTION
       end
 
-      array_of_documents_as_strings.each do |document|
+      array_of_strings.each do |document|
         if !document.is_a?(String) || document.blank?
           raise Analyzer::WRONG_INPUT_EXCEPTION
         end
       end
 1   end
 
-    def calculate_vector_space(document_vector)
-      #TODO: I have to find out how the vector should be formatted since each document may have different words that don't appear in other, so one position may have a different meaning if the terms are different
-
-      # See the distances paper and think about an algorithm that generates an array whose size be the total amount of different terms in the corpus and each position belongs to a certain term
-      # Then, the vector space should contain an array of same proportions in which each position be the tf_idf value of each term for that document
-      # STUDY ABOUT TF IDF TO ASSERT THAT THE PREVIOUS NOTE MAKES SENSE (IS TF_IDF A METRIC OF A TERM OR A METRIC FOR THE WHOLE DOCUMENT  ?????????????) - CHECK ALSO K MEANS DOCUMENTATION
-
+    def calculate_vector_space!(document_vector)
+      vector_space = Array.new
+      @set_of_terms.each_with_index do |term, index|
+        vector_space[index] = tf_idf(document_vector, term)
+      end
+      document_vector.vector_space = vector_space
     end
 
     def tf_idf(document_vector, term)
