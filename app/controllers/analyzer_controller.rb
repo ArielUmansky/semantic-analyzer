c93315354b87c765aeb2bceacde9b9a3fb82ec35
Ariel <umanskyariel@gmail.com>
2015-09-06 04:50:56 -0300

    Implements the analyzer controller

--- app/controllers/analyzer_controller.rb
+++ app/controllers/analyzer_controller.rb
@@ -1,7 +1,22 @@
 class AnalyzerController < ApplicationController
 
+
   def perform
-    Analyzer.perform(request.body)
+    begin
+      check_params
+      analyzer = Analyzer.new
+      result_set = analyzer.perform(params[:body][:corpus], params[:body][:algorithm], params[:body][:metadata])
+      render json: { result_set: result_set, algorithm: analyzer.algorithm }
+    rescue ActionController::ParameterMissing, RuntimeError
+      render :nothing => true, :status => 400
+    end
+  end
+
+  private
+
+  def check_params
+     params.require(:body).require(:corpus)
+     params.require(:body).permit(:algorithm, :metadata)
   end
 
 end
