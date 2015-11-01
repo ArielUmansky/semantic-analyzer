134554a5a5ca3f9d733e93fc5c13f652a1f8f99e
Ariel <umanskyariel@gmail.com>
2015-11-01 00:50:54 -0300

    Adds error managment inside the async job

--- app/jobs/analyzer_job.rb
+++ app/jobs/analyzer_job.rb
@@ -4,9 +4,13 @@ class AnalyzerJob
   include SuckerPunch::Job
 
   def perform(corpus, algorithm, metadata, url)
-    analyzer = Analyzer.new
-    result_set = analyzer.perform(corpus, algorithm, metadata)
-    json_result = { result_set: analyzer.presenter_class.new(result_set), algorithm: analyzer.algorithm }
-    Requests.post(url, data: json_result.to_json, headers: {"Content-type" => "application/json"} )
+    begin
+      analyzer = Analyzer.new
+      result_set = analyzer.perform(corpus, algorithm, metadata)
+      json_result = { result_set: analyzer.presenter_class.new(result_set), algorithm: analyzer.algorithm }
+      Requests.post(url, data: json_result.to_json, headers: {"Content-type" => "application/json"} )
+    rescue RuntimeError => e
+      Requests.post(url, data: { error: e.message }.to_json, headers: {"Content-type" => "application/json"} )
+    end
   end
 end
